7cb2c75e0dd72ab2d2dd774c43cbcb045364edef
Ankit <ankitchauhan21500@gmail.com>
2022-12-19 17:33:50 +0530

    added new project

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let array1 = ["tree", "toy", "doll", "chair", "mother", "peace", "camel", "horse", "family", "friend", "pony", "jump", "forest", "deer", "clouds", "earth", "moon", "sun", "jelly", "bees", "monkey", "bull"];
+let word = document.querySelector('.word');
+let moon = document.querySelector('.moon');
+let list = document.querySelector('.list');
+let bar = document.querySelector('.bar');
+let reveal = document.querySelector('.reveal');
 var myTimeout;
 function makearray(str) {
 	let array = []; let string = "";
@@ -13,7 +19,10 @@ function makearray(str) {
 	return array;
 }
 let newArray = [];
+let array = [];
+let l;
 newArray = makearray("attack attend bicycle breakfast brightly cabbage cable carpenter channel circle climb comfort comical confirm construct curtain customer damage decide delight disappear discover empty encourage entertain equal exactly forever fruit fuel group guard guest guide guitar handle health heart heavily helmet idea kindness level locket lumber magic melon meter money motor mountain partner perfect perhaps personal plastic pocket protect provide railways record reward shoulder socket stranger stroll subject suit supply temper theatre total toward treatment useful vacant windy writer");
+let hardArray = makearray("abash abate abdicate aberration abstain abstruse acknowledgment adequate adjudicate adroit adversity amicable analogous annul applause apprehension aquatic arbitrary arid aristocracy articulation aspiration assessment assimilate asylum available avert basin bemoan benevolent bewildered bias boisterous boondoggle brazen brusque canny capability capacious capitulate caveat chaff chronic circumvent clairvoyant classic cognitive coherence collaborate combat commemorate commission comply concept concomitant condescending condition conjunction conspicuous constrain context controversy corollary corrugated covert decipher defunct delineate diversity dominant effective efficacy efficient elliptical eloquent embellish emission encompass endow engender enhancement enormous environment ethnic eventually evident expanse explicit export extravagant facilitate fiduciary finance framework frugality gregarious habitat harass harassment hereditary heritage hone hybrid illegitimate immerse immigrant imperative implicit improbable inalienable incident income indict indigenous infrastructure inimical innovative integrate intransigence jurisdiction jurisprudence kaleidoscope laud laudatory legacy legislate livelihood malediction mandate mayhem methodology migratory motivate muster")
 function myTimer() {
 	window.speechSynthesis.pause();
 	window.speechSynthesis.resume();
@@ -24,15 +33,15 @@ function sound(text) {
 	var utt = new SpeechSynthesisUtterance(text);
 	utt.voice = window.speechSynthesis.getVoices().find(voice => voice.name === 'Alice');
 	utt.pitch = 1;
-	utt.rate = 1;
+	utt.rate = .8;
 	utt.onend = function () { clearTimeout(myTimeout); }
 	window.speechSynthesis.speak(utt);
 }
-async function greeting( score , l){
+async function greeting(score, l) {
 	sound(`Welcome here your current score is ${score} out of ${l}`);
 }
-async function sessionmem(){
-	sessionStorage.setItem('greet' , "greetend");
+async function sessionmem() {
+	sessionStorage.setItem('greet', "greetend");
 }
 function Scoring(num, coloring) {
 	if (num > 0) {
@@ -51,49 +60,98 @@ let total = document.querySelector('.total');
 let points = document.querySelector('.points');
 let repeat = document.querySelector('.repeat');
 let next = document.querySelector('.next');
+let clear = document.querySelector('.clear');
 let scorepoints = localStorage.getItem("score");
 let index = localStorage.getItem('index');
 let score;
 let i;
-if (index == null) {
-	i = 0;
-	score = 0;
-	points.innerHTML = 0;
-	Scoring(score, points)
-}
-else {
-	i = parseInt(index);
-	score = parseInt(scorepoints)
-	points.innerHTML = scorepoints;
-	Scoring(score, points)
-}
 let sesiongreet = sessionStorage.getItem("greet");
 myTimeout = setTimeout(myTimer, 10000);
-let array = ["tree", "toy", "doll", "chair", "mother", "peace", "camel", "horse", "family", "friend", "pony", "jump", "forest", "deer", "clouds", "earth", "moon", "sun", "jelly", "bees", "monkey", "bull"];
-let l = array.length;
-if (sesiongreet === null) {
-	async function outgreeting(){
-		await greeting(score , l);
-		await sessionmem();
-	}   
-	outgreeting();
-}
-total.innerHTML = `/${l}`;
+
+let level = document.querySelectorAll(".level");
+level.forEach((element, index) => {
+	element.addEventListener("click", function () {
+		word.style.color = 'transparent';
+		if (index === 0) {
+			let indexNumber = localStorage.getItem("index1");
+			let Score = localStorage.getItem("score1");
+			if (indexNumber === null) {
+				i = 0;
+				score = 0;
+			}
+			else {
+				i = parseInt(indexNumber);
+				score = parseInt(Score);
+			}
+			array = array1;
+			l = array.length;
+			word.innerHTML = array[i];
+			points.innerHTML = score;
+		}
+		else if (index === 1) {
+			let indexNumber1 = localStorage.getItem("index2");
+			let Score1 = localStorage.getItem("score2");
+			if (indexNumber1 === null) {
+				i = 0;
+				score = 0;
+			}
+			else {
+				i = parseInt(indexNumber1);
+				score = parseInt(Score1);
+			}
+			array = newArray;
+			l = array.length;
+			word.innerHTML = array[i];
+			points.innerHTML = score;
+		}
+		else {
+			let indexNumber2 = localStorage.getItem("index3");
+			let Score2 = localStorage.getItem("score3");
+			if (indexNumber2 === null) {
+				i = 0;
+				score = 0;
+			}
+			else {
+				i = parseInt(indexNumber2);
+				score = parseInt(Score2);
+			}
+			array = hardArray;
+			l = array.length;
+			word.innerHTML = array[i];
+			points.innerHTML = score;
+		}
+		total.innerHTML = `/${l}`;
+	})
+});
+
 input1.addEventListener("click", function () {
-	let word = array[i].charAt(0).toUpperCase() + array[i].slice(1);
-	if ((input.value === array[i] || input.value === word) && array[i] != undefined) {
-		score++;
-		Scoring(score, points);
-		i++;
+	word.style.color = 'transparent';
+	let word1 = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+	if ((input.value === array[i] || input.value === word1) && array[i] != undefined) {
 		if (array[i] === undefined) {
 			sound(`You have completed the test, congratulations!!`);
 		}
 		else {
-			localStorage.setItem("index", i);
-			localStorage.setItem("score", score);
+			i++;
+			score++;
+			Scoring(score, points);
+			if (array[0] === 'tree') {
+				localStorage.setItem("index1", i);
+				localStorage.setItem("score1", score);
+			}
+			else if (array[0] === 'attack') {
+				localStorage.setItem("index2", i);
+				localStorage.setItem("score2", score);
+			}
+			else {
+				localStorage.setItem("index3", i);
+				localStorage.setItem("score3", score);
+			}
 			sound(`Right answer, and The next word is ${array[i]}`);
 		}
+		input.value = "";
 		points.innerHTML = score;
+		word.innerHTML = array[i];
 	}
 	else {
 		if (array[i] === undefined) {
@@ -104,35 +162,60 @@ input1.addEventListener("click", function () {
 		}
 	}
 })
-// localStorage.clear();
-repeat.addEventListener("click", function () {
+
+reveal.addEventListener("click", function () {
+	word.style.color = 'var(--blue)';
+})
+
+
+clear.addEventListener("click", function () {
+	localStorage.clear();
+	word.style.color = 'transparent';
 	if (array[i] === undefined) {
-		sound(`You have completed the test, congratulations!!`);
+		sound("Please select a level and start the game");
+	}
+	else {
+		score = 0;
+		i = 0;
+		input.value = "";
+		points.innerHTML = score;
+		Scoring(score, points);
+		sound(` RE-starting the game`);
+		word.innerHTML = array[i];
+	}
+})
+
+
+
+
+
+repeat.addEventListener("click", function () {
+	word.style.color = 'transparent';
+	if (array[i] === undefined) {
+		sound(`Please select a level and start the game!`);
 	}
 	else {
 		sound(`The current word is ${array[i]}`);
 	}
 })
 
-let moon = document.querySelector('.moon');
-let list = document.querySelector('.list');
-let bar = document.querySelector('.bar');
-let check=1;
+let check = 1;
 moon.addEventListener('click', function (e) {
-    if (check % 2 != 0) {
-        moon.innerHTML = '☀️';
-        document.body.style.color = 'white';
-        document.body.style.background = 'black';
-        bar.style.border = '3px solid white';
-        points.style.color = 'white';
+	word.style.color = 'transparent';
+	if (check % 2 != 0) {
+		moon.innerHTML = '☀️';
+		document.body.style.color = 'white';
+		document.body.style.background = 'black';
+		bar.style.border = '3px solid white';
+		points.style.color = 'white';
 		check++;
-    }
-    else {
+	}
+	else {
 		moon.innerHTML = '🌑';
-        document.body.style.color = 'black';
-        document.body.style.background = 'white';
-        bar.style.border = '3px solid black';
-        points.style.color = 'black';
+		document.body.style.color = 'black';
+		document.body.style.background = 'white';
+		bar.style.border = '3px solid black';
+		points.style.color = 'black';
 		check++;
 	}
 })
